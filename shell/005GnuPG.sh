127165e8fb11df291eaf07d2d232de78ca6932ad
Pinaki Sekhar Gupta <appugupta007@gmail.com>
2020-11-18 23:47:11 +0530

    Add files via upload

--- shell/005GnuPG.sh
+++ shell/005GnuPG.sh
@@ -5,5 +5,10 @@
 # sudo apt install kleopatra \
 # Optional (Warning: kleopatra Requires KDE dependencies and 117MB extra space on the drive)
 
-sudo apt install gpa \
+sudo apt install gpa && \
+sudo apt install libccid && \
+sudo apt install opensc-pkcs11 && \
+sudo apt install pcsc-tools && \
+sudo apt install pcscd && \
+sudo apt install opensc \
 
