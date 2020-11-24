b8903bd224909d1b7ca8285259464177c1f3518c
Pinaki Sekhar Gupta <appugupta007@gmail.com>
2020-11-24 18:07:38 +0530

    Add files via upload

--- shell/009video-n-vfx.sh
+++ shell/009video-n-vfx.sh
@@ -8,8 +8,42 @@ sudo snap install handbrake-jz && \
 
 # Only download what you need
 
-# sudo apt install kdenlive && \
-# sudo apt install shotcut && \
+# sudo snap install kdenlive && \
+# sudo snap install shotcut && \
+
+# Or, go for Cinelerra-GG Infinity
+
+# https://www.cinelerra-gg.org
+# Download a static Single-user build
+# https://www.cinelerra-gg.org/downloads
+# Source Code: https://github.com/cinelerra-gg
+
+# Create a desktop entry: Cinelerra-GG Infinity
+# As:
+
+# =================================
+# #!/usr/bin/env xdg-open
+
+# [Desktop Entry]
+# X-AppInstall-Package=cin
+# X-AppInstall-Popcon=1158
+# X-AppInstall-Section=main
+
+# Name=Cinelerra-GG Infinity
+# GenericName=Video Editor
+# Comment=Video Edititing Program
+# TryExec='/home/YourUserName/cinelerra-5.1-ub20.04-20201031.x86_64-static/cin'
+# Exec='/home/YourUserName/cinelerra-5.1-ub20.04-20201031.x86_64-static/cin'
+# Terminal=false
+# Type=Application
+# Icon=/home/YourUserName/cinelerra-5.1-ub20.04-20201031.x86_64-static/CinGG.png
+# Categories=Multimedia;VideoEditor;
+# StartupNotify=true
+# MimeType=;
+
+# X-Ubuntu-Gettext-Domain=app-install-data
+# Path='/home/YourUserName/cinelerra-5.1-ub20.04-20201031.x86_64-static/'
+# =================================
 
 # Online Graphic Design (Software As A Service)
 # https://videobolt.net/welcome
---
+++ shell/c-c-plus-plus-common-libraries.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Boost C++ libraries
+
+sudo apt install libboost-all-dev && \
+
+# OpenGL libraries
+
+sudo apt install libglu1-mesa-dev && \
+sudo apt install freeglut3-dev && \
+sudo apt install mesa-common-dev && \
+sudo apt install libglew-dev && \
+sudo apt-get install binutils && \
+sudo apt install libglm-dev && \
+sudo apt install libgl-dev && \
+sudo apt install libglew-dev \
+
+# Other libraries to consider:
+
+# openmp
+# opencv2
+# tinyxml2
+# zlib
+# sdl
+# sfml
+# posix
+# openssl
+# libsigc++
+# libcerror
+
