eaa30ecb34fa620735f3163f5c49fe629d3976b9
Pinaki Sekhar Gupta <appugupta007@gmail.com>
2020-12-17 15:17:40 +0530

    Update c-c-plus-plus-common-libraries.sh

--- shell/c-c-plus-plus-common-libraries.sh
+++ shell/c-c-plus-plus-common-libraries.sh
@@ -19,11 +19,16 @@ sudo apt install libglew-dev && \
 
 sudo apt install libfltk1.3-dev && \
 sudo apt install libfltk1.3-compat-headers && \
-sudo apt install pinentry-fltk \
+sudo apt install pinentry-fltk && \
+
+# Some useful libraries
+
+sudo apt install libtinyxml2-dev && \
+sudo apt install zlib1g-dev && \
+sudo apt install libtinyxml2-dev && \
 
 # Other libraries to consider:
 
-# openmp
 # opencv2
 # tinyxml2
 # zlib
