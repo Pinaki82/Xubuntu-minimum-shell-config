9e2d6917330d568aa619884f032d8721e758bf28
Pinaki Sekhar Gupta <appugupta007@gmail.com>
2020-12-29 01:36:23 +0530

    Update 014chromium-beta.sh

--- shell/014chromium-beta.sh
+++ shell/014chromium-beta.sh
@@ -13,3 +13,7 @@ sudo apt install chromium-browser && \
 sudo apt install -y chromium-codecs-ffmpeg && \
 sudo apt install -y chromium-codecs-ffmpeg-extra \
 
+# uninstall
+# sudo apt purge --auto-remove chromium-browser
+# sudo apt purge --auto-remove chromium-codecs-ffmpeg-extra
+# sudo ppa-purge ppa:saiarcot895/chromium-beta
