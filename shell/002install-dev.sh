9ed855218ea71680cfe0b34dc5d2d7492d20e26b
Pinaki Sekhar Gupta <appugupta007@gmail.com>
2021-01-05 22:26:02 +0530

    Update 002install-dev.sh

--- shell/002install-dev.sh
+++ shell/002install-dev.sh
@@ -9,6 +9,7 @@ sudo apt install libomp-dev && \
 sudo apt install cdecl && \
 sudo apt install cutils && \
 sudo apt install kitty && \
+sudo apt install mc && \
 sudo apt install vim-gtk && \
 sudo apt install cppcheck && \
 sudo apt install cppcheck-gui && \
