dcfd1318f62d48f7a546c7026249fca6b7bdffab
Pinaki Sekhar Gupta <appugupta007@gmail.com>
2021-01-07 18:33:56 +0530

    Add files via upload

---
+++ shell/shell-scripts-updater.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ~/  && \
+rm -rf ~/Xubuntu-minimum-shell-config && \
+mv ~/shell/operation-timeshift.sh ~/
+git clone https://github.com/Pinaki82/Xubuntu-minimum-shell-config.git  && \
+rm -rf ~/Xubuntu-minimum-shell-config/.git  && \
+rm ~/Xubuntu-minimum-shell-config/README.md ~/Xubuntu-minimum-shell-config/LICENSE ~/Xubuntu-minimum-shell-config/PortableAppInfo.txt && \
+# https://www.tecmint.com/sync-new-changed-modified-files-rsync-linux/
+# https://unix.stackexchange.com/questions/501975/why-does-rsync-keep-skipping-the-directory-i-want-to-sync
+rsync --archive --recursive --verbose --progress ~/Xubuntu-minimum-shell-config/shell/* ~/shell && \
+# rsync --archive --recursive --verbose --progress ~/Tulu-C-IDE/.vimdotcommon// ~/.vimdotcommon &&\
+# rsync --archive --recursive --update --verbose --progress ~/Tulu-C-IDE/.vimdotlinux// ~/.vimdotlinux &&\
+rsync --archive --verbose --progress ~/operation-timeshift.sh ~/shell/operation-timeshift.sh && \
+rm -rf ~/Xubuntu-minimum-shell-config && \
+rm ~/operation-timeshift.sh \
+
