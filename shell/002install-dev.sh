810b5d9ea52d55cdabca360e4c8e3182e251721b
Pinaki Sekhar Gupta <appugupta007@gmail.com>
2021-01-11 02:34:54 +0530

    Update 002install-dev.sh

--- shell/002install-dev.sh
+++ shell/002install-dev.sh
@@ -9,6 +9,7 @@ sudo apt install libomp-dev && \
 sudo apt install cdecl && \
 sudo apt install cutils && \
 sudo apt install kitty && \
+sudo apt install tree && \
 sudo apt install mc && \
 # https://itsfoss.com/vim-8-release-install/
 sudo add-apt-repository ppa:jonathonf/vim && \
