9df3b2807d9fdcf9265f8efc79b2997abbc55d42
Pinaki Gupta <appugupta007@gmail.com>
2021-02-08 21:34:22 +0530

    KeePassXC's repository information updated

--- shell/004xubuntu-authentication.sh
+++ shell/004xubuntu-authentication.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# sudo snap install authy --beta  && \
-# sudo snap install keepassxc \
-# Download keepassxc and MYKI AppImage instead
 
-sudo snap install authy --beta \
+# sudo snap install authy --beta  && \
+# sudo add-apt-repository ppa:phoerious/keepassxc && \
+# sudo apt install keepassxc \
+
+# NOTE:
+# Download KeePassXC and MYKI AppImage instead, if you do not need
+# KeePassXC's SSH integration and CLI
 
