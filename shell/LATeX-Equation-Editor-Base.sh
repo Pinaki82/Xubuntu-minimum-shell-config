07bff4a8a7da0b0def373df4eed6de1142c7ba23
Pinaki Gupta <appugupta007@gmail.com>
2021-03-06 14:36:27 +0530

    Installation scripts for LATeX equation editors + LATeX base added

---
+++ shell/LATeX-Equation-Editor-Base.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# WYSIWYG LATeX Equation Editors
+
+# References:
+# https://equalx.sourceforge.io/
+# https://www.thrysoee.dk/laeqed/
+
+sudo apt install texlive-base && \
+sudo apt install dvipng && \
+sudo apt install dvisvgm && \
+sudo apt install ghostscript && \
+sudo apt install equalx && \
+# JVM Runtime is required by Laeqed
+sudo apt install default-jre \
+
+# On MS Windows, use MiKTeX (https://miktex.org/),
+# Ghostscript (https://www.ghostscript.com/),
+# EqualX (https://equalx.sourceforge.io/),
+# Laeqed (https://www.thrysoee.dk/laeqed/).
+
