83f7115cd5e5c7ebf00c9ef079b751dbe18653a9
Pinaki Gupta <appugupta007@gmail.com>
2021-04-02 23:09:51 +0530

    1. Softmaker FreeOffice added to the list of portable apps. 2. Otter Browser listed as an essential package and added to an installation script.

--- shell/003xubuntu-essential.sh
+++ shell/003xubuntu-essential.sh
@@ -40,6 +40,9 @@ sudo apt install appimagelauncher && \
 sudo apt install gkrellm && \
 sudo apt install torbrowser-launcher && \
 sudo apt install epiphany-browser && \
+sudo add-apt-repository ppa:otter-browser/release && \
+sudo apt update && \
+sudo apt install otter-browser && \
 sudo snap install goldendictionary && \
 sudo apt install artha && \
 sudo apt install xchm && \
