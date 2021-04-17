3ffe27cb48cad88af3b9b80023ec172162e448e3
Pinaki Gupta <appugupta007@gmail.com>
2021-04-18 03:59:10 +0530

    1. ARM-GCC installation guide updated (OpenOCD added) 2. Checked compilation of a sample ARM-GCC makefile project 3. Deleted invalid instructions from the ARM-GCC installation guide 4. Desktop entries created for quickly opening useful websites and search engines/aggregators 5. config.fish updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-set PATH $PATH /usr/local/bin ~/gcc-arm-none-eabi-10-2020-q4-major/bin/ ~/DrMemory-Linux-2.3.18722/bin64/
+set PATH $PATH /usr/local/bin ~/DrMemory-Linux-2.3.18722/bin64/
 export PATH
 function ll
     ls -lh $argv
