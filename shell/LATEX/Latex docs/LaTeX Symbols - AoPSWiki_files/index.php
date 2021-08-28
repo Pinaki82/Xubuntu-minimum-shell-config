bb63222048b5fcdda45f8f2d4107591a16187e98
Pinaki Gupta <appugupta007@gmail.com>
2021-08-28 21:29:06 +0530

    'Latex docs' has been compressed to a zip archive.

--- shell/LATEX/Latex docs/LaTeX Symbols - AoPSWiki_files/index.php
+++
@@ -1,8 +0,0 @@
-/* generated javascript */
-var skin = 'aops';
-var stylepath = '/Wiki/skins';
-
-/* MediaWiki:Common.js */
-/* Any JavaScript here will be loaded for all users on every page load. */
-
-/* MediaWiki:Aops.js */
