f649ce42fb7b8f2cac89d692a031a4fb003b33ef
Pinaki <appugupta007@gmail.com>
2021-09-15 19:55:36 +0530

    Blender PPA updated.

--- shell/009video-n-vfx.sh
+++ shell/009video-n-vfx.sh
@@ -76,8 +76,8 @@ sudo apt install winff-doc \
 
 # Blender 3D Animation package can be installed as a snap app
 
-# sudo add-apt-repository ppa:thomas-schiex/blender && \
-# https://ubuntuhandbook.org/index.php/2021/03/install-blender-2-92-ubuntu-20-04-20-10-via-ppa/
+# sudo add-apt-repository ppa:savoury1/blender && \
+# https://ubuntuhandbook.org/index.php/2021/06/install-blender-2-93-ppa-ubuntu-20-04/
 # sudo apt update && \
 # sudo apt install blender && \
 
