ac6fe65be1141bab631677ea927e4fede11de12d
Pinaki <appugupta007@gmail.com>
2021-11-17 20:56:10 +0530

    Information about some software packages which are available in AppImage format has been updated. 1. FreeCAD 2. OpenSCAD 3. LibrePCB 4. Nextcloud

--- shell/012hardware-design.sh
+++ shell/012hardware-design.sh
@@ -2,6 +2,11 @@
 
 sudo apt install kicad && \
 sudo snap install freecad && \
+# Also available in AppImage package.
+# FreeCAD (Parametric 3D CAD Modeler) -> https://www.freecadweb.org/
 sudo snap install openscad && \
+# Also available in AppImage package.
+# Script file based graphical CAD environment
+# http://openscad.org/downloads.html#linux
 sudo apt install sweethome3d \
 
