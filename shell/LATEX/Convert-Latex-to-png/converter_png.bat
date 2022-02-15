831adfeb79ca189eb0fdf6eb4f53643a5b25180b
Pinaki Gupta <appugupta007@gmail.com>
2022-02-15 13:02:07 +0530

    Fixed some UTF-8 encoding error in a LaTeX supplementary file. Changed the Dot Per Inch settings in scripts intended for converting TeX to PNG.

--- shell/LATEX/Convert-Latex-to-png/converter_png.bat
+++ shell/LATEX/Convert-Latex-to-png/converter_png.bat
@@ -6,8 +6,8 @@ path=%APPDATA%\Local\Programs\MiKTeX\miktex\bin\x64\;C:\Program Files\gs\gs9.53.
 latex int01.tex
 latex eq001.tex
 
-dvipng -T tight -D 6000 --bdpi 2400 -z 9 -bg Transparent -quality 90 -o int01.png int01.dvi
-dvipng -T tight -D 6000 --bdpi 2400 -z 9 -bg Transparent -quality 90 -o eq001.png eq001.dvi
+dvipng -T tight -D 300 --bdpi 300 -z 9 -bg Transparent -quality 90 -o int01.png int01.dvi
+dvipng -T tight -D 300 --bdpi 300 -z 9 -bg Transparent -quality 90 -o eq001.png eq001.dvi
 
 del *.aux *.dvi *.log
 cmd
--- shell/LATEX/Convert-Latex-to-png/mybook.md
+++ shell/LATEX/Convert-Latex-to-png/mybook.md
@@ -9,3 +9,4 @@ Chapter two has just begun.
 $\oint \vec{F} \cdot d\vec{s}=0$
 
 Here's, $\mu = \sum_{i=0}^{N} \frac{x_i}{N}$, the mean equation, ...
+
